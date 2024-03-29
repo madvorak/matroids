13992271435dcbbab5e7e47c8ebc36dcf7e8dd1c
madvorak <martin.dvorak@matfyz.cz>
2024-03-29 21:16:06 +0100

    first day of our collaboration

---
+++ lakefile.lean
@@ -0,0 +1,14 @@
+import Lake
+open Lake DSL
+
+package matroids {
+  moreServerOptions := #[⟨`autoImplicit, false⟩, ⟨`pp.structureInstances, false⟩]
+}
+
+require mathlib from git
+  "https://github.com/leanprover-community/mathlib4.git"
+
+@[default_target]
+lean_lib Matroids {
+  moreLeanArgs := #["-DautoImplicit=false"]
+}
