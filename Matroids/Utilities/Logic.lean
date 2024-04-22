5c50c8ee480af526b667fe6bb3297b5c5ec1d316
madvorak <martin.dvorak@matfyz.cz>
2024-04-22 14:07:31 +0200

    forall_forall_and_iff is equivalence

--- Matroids/Utilities/Logic.lean
+++ Matroids/Utilities/Logic.lean
@@ -1,4 +1,5 @@
 
-theorem forall_and_forall {α : Type} (P Q : α → Prop) [Inhabited α] (hPQ : ∀ a b, P a ∧ Q b) :
-    (∀ a, P a) ∧ (∀ b, Q b) :=
-  ⟨fun n => (hPQ n default).left, fun m => (hPQ default m).right⟩
+theorem forall_forall_and_iff {α β : Type} (P : α → Prop) (Q : β → Prop) [Inhabited α] [Inhabited β] :
+    (∀ a b, P a ∧ Q b) ↔ (∀ a, P a) ∧ (∀ b, Q b) :=
+  ⟨fun hpq => ⟨fun a : α => (hpq a default).left, fun b : β => (hpq default b).right⟩,
+  fun ⟨hp, hq⟩ => fun a : α => fun b : β => ⟨hp a, hq b⟩⟩
