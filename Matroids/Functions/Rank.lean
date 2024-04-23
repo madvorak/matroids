85a8a2161fcc14233defdb5b979df74b9314076a
madvorak <martin.dvorak@matfyz.cz>
2024-04-23 19:32:32 +0200

    some easy stuff

--- Matroids/Functions/Rank.lean
+++ Matroids/Functions/Rank.lean
@@ -1,6 +1,7 @@
 import Mathlib.Data.ENat.Basic
 import Mathlib.Order.SetNotation
 import Matroids.IndepMatroid_aug
+import Mathlib.Tactic.Have
 
 
 variable {α : Type*}
@@ -12,37 +13,36 @@ noncomputable def IndepMatroid.relativeRank (M : IndepMatroid α) {A B : Set α}
       Set.encard (I.val \ J.val)
 
 /-- relative rank of empty set and empty set is zero -/
-lemma relativeRank_empty_empty (M : IndepMatroid α) :
+lemma relativeRank_empty_empty {M : IndepMatroid α} :
     M.relativeRank (Set.empty_subset _) (Set.empty_subset _) = 0 := by
   sorry
 
 /-- relative rank of two sets is at most cardinality of their difference -/
-lemma relativeRank_le_encard (M : IndepMatroid α) {A B : Set α} (hA : A ⊆ M.E) (hB : B ⊆ A) :
+lemma relativeRank_le_encard {M : IndepMatroid α} {A B : Set α} (hA : A ⊆ M.E) (hB : B ⊆ A) :
     M.relativeRank hA hB ≤ Set.encard (A \ B) := by
   sorry
 
 /-- relative rank with intersection in second argument is at least relative rank with union in first argument -/
-lemma relativeRank_inter_ge_union (M : IndepMatroid α) {A B : Set α} (hA : A ⊆ M.E) (hB : B ⊆ M.E) :
+lemma relativeRank_inter_ge_union {M : IndepMatroid α} {A B : Set α} (hA : A ⊆ M.E) (hB : B ⊆ M.E) :
     M.relativeRank hA (Set.inter_subset_left A B) ≥
     M.relativeRank (Set.union_subset hA hB) (Set.subset_union_right A B) := by
   sorry
 
 /-- relative rank decomposes into a sum of relative ranks with an intermediate set -/
-lemma relativeRank_telescope (M : IndepMatroid α) {A B C : Set α} (hA : A ⊆ M.E) (hB : B ⊆ A) (hC : C ⊆ B) :
+lemma relativeRank_telescope {M : IndepMatroid α} {A B C : Set α} (hA : A ⊆ M.E) (hB : B ⊆ A) (hC : C ⊆ B) :
     M.relativeRank hA (hC.trans hB) = M.relativeRank hA hB + M.relativeRank (hB.trans hA) hC := by
   sorry
 
 /-- if relative rank is zero with set family in first argument, it is zero with union of that family -/
-lemma relativeRank_family_zero (M : IndepMatroid α) {ι : Type} {{A : ι → Set α}} {B : Set α}
+lemma relativeRank_family_zero {M : IndepMatroid α} {ι : Type} [Inhabited ι] {A : ι → Set α} {B : Set α}
     (hA : ∀ i : ι, A i ⊆ M.E) (hB : ∀ i : ι, B ⊆ A i) (hR : ∀ i : ι, M.relativeRank (hA i) (hB i) = 0) :
-    M.relativeRank (Set.iUnion_subset hA) (show B ⊆ Set.iUnion A by sorry) = 0 := by
+    M.relativeRank (Set.iUnion_subset hA) ((hB default).trans (Set.subset_iUnion_of_subset default (subset_refl _))) =
+      0 := by
   sorry
 
 /-- set of independent sets defined via relative rank function -/
 def IndepMatroid.rankIndepSets (M : IndepMatroid α) : Set (Set α) :=
-  {I : Set α | I ⊆ M.E ∧ ∀ x ∈ I,
-    M.relativeRank (show I ⊆ M.E by sorry) (show I \ {x} ⊆ I by sorry) > 0}
-   -- todo @Martin: refactor condition above to say "r(I | I - x) > 0", but nicely
+  { (I) | (I : Set α) (hI : I ⊆ M.E) (_ : ∀ x ∈ I, M.relativeRank hI (Set.diff_subset I {x}) > 0) }
 
 /-- set of r-independent sets satisfies maximal property (extension to a maximal) -/
 lemma rankIndepSets_have_maximals (M : IndepMatroid α) :
@@ -77,7 +77,7 @@ lemma absoluteRank_of_finite_le (M : IndepMatroid α) (hM : Set.Finite M.E)
 /-- for finite matroids, absolute rank is submodular -/
 lemma absoluteRank_of_finite_submodular (M : IndepMatroid α) (hM : Set.Finite M.E)
     {A B : Set α} (hA : A ⊆ M.E) (hB : B ⊆ M.E) :
-    M.absoluteRank (Set.union_subset hA hB) + M.absoluteRank (show A ∩ B ⊆ M.E by sorry) ≤
+    M.absoluteRank (Set.union_subset hA hB) + M.absoluteRank ((Set.inter_subset_left A B).trans hA) ≤
       M.absoluteRank hA + M.absoluteRank hB := by
   sorry
 
